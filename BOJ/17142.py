9a1bf2898b422fbe79268c0239782f05f6950e3e
Hansol <azder43@gmail.com>
2022-01-24 12:26:58 +0900

    2022.01.24

--- BOJ/17142.py
+++ BOJ/17142.py
@@ -1,16 +1,52 @@
 # https://www.acmicpc.net/problem/17142
-# 백준 17142/ 연구소3(삼성 기출)
+# 백준 17142/ 연구소3(삼성 기출) /골드4
 # 2022.01.22
+from copy import deepcopy
+from collections import deque
+
+def bfs(flag):
+    global ans
+    cnt = 0
+    q = deque()
+    new_board = deepcopy(board)
+    for i in flag:
+        q.append((virus[i][0],virus[i][1]))
+        new_board[virus[i][0]][virus[i][1]] = 10
+    while q:
+        x, y = q.popleft()
+        for dx,dy in [[0,1],[-1,0],[0,-1],[1,0]]:
+            nx,ny = x + dx, y +dy
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+                continue
+            if new_board[nx][ny] == 0:
+                new_board[nx][ny] = new_board[x][y] + 1
+                q.append([nx,ny])
+                cnt += 1
+            if new_board[nx][ny] == 2:
+                new_board[nx][ny] = new_board[x][y] + 1
+                q.append([nx, ny])
+        if cnt == blankcnt:
+            time = 0
+            for i in new_board:
+                time = max(max(i), time)
+            ans = min(time, ans)
+            break
+    else:
+        ans = min(2500, ans)
 
 
-def bfs():
-    pass
 
-def seletVirus(cnt, flag):
-    if cnt == m:
-        bfs(flag)
+def seletVirus(idx, selectvirus):
+    if len(selectvirus) == m:
+        bfs(selectvirus)
         return
 
+    for i in range(idx, viruscnt):
+        if viruscnt - idx < m - len(virus): ## 남은 바이러스를 다 선택해도 m개를 채우지 못할떄
+            continue
+        selectvirus.append(i)
+        seletVirus(i +1, selectvirus)
+        selectvirus.pop()
 
 n, m = map(int, input().split())
 board = [list(map(int,input().split(' '))) for _ in range(n)]
@@ -25,10 +61,15 @@ for i in range(n):
             virus.append([i,j])
 viruscnt = len(virus)
 blankcnt = n * n - wallcnt - viruscnt
-virusflag = [False * viruscnt]
+selectvirus = []
 
-seletVirus(0, virusflag)
 
+ans = 2500
+seletVirus(0, selectvirus)
+if blankcnt == 0:
+    print(0)
+else:
+    print(-1 if ans == 2500 else ans-10)
 
 
 
---
+++ BOJ/17837.py
@@ -0,0 +1,98 @@
+# https://www.acmicpc.net/problem/17837
+# 백준17837/ 새로운게임2/ 골드2
+# 2022.01.23
+
+def play():
+    cnt = 0
+    dir = [[0,1],[0,-1],[-1,0],[1,0]] ##동서북남
+    while cnt <= 1000:
+        cnt+=1
+        for x,y,d,idx in mal:
+            nx, ny = x + dir[d][0], y + dir[d][1]
+            if nx <0 or ny < 0 or nx >= n or ny >= n or board[nx][ny] == 2:
+                nd = d + 1 if d % 2 == 0 else d - 1
+                nx, ny = x + dir[nd][0], y + dir[nd][1]
+                if nx <0 or ny < 0 or nx >= n or ny >= n or board[nx][ny] == 2:
+                    nx,ny,nd = x, y, nd
+                    # for i in stackboard:
+                    #     print(i)
+                    # print(mal)
+                    # input(idx)
+                elif board[nx][ny] == 1:
+                    mal[idx] = [nx, ny, nd, idx]
+                    for i in range(len(stackboard[x][y])):
+                        if stackboard[x][y][i] == idx:
+                            for j in stackboard[x][y][i:]:
+                                mal[j][0], mal[j][1] = nx, ny
+                            plus = stackboard[x][y][i:]
+                            stackboard[nx][ny] += plus[::-1]
+                            stackboard[x][y] = stackboard[x][y][0:i]
+                            break
+                    # for i in stackboard:
+                    #     print(i)
+                    # print(mal)
+                    # input(idx)
+                elif board[nx][ny] == 0:
+                    for i in range(len(stackboard[x][y])):
+                        if stackboard[x][y][i] == idx:
+                            for j in stackboard[x][y][i:]:
+                                mal[j][0], mal[j][1] = nx, ny
+                            stackboard[nx][ny] += stackboard[x][y][i:]
+                            stackboard[x][y] = stackboard[x][y][0:i]
+                            break
+                    # for i in stackboard:
+                    #     print(i)
+                    # print(mal)
+                    # input(idx)
+                mal[idx] = [nx,ny,nd,idx]
+
+            elif board[nx][ny] == 0:
+                mal[idx] = [nx, ny, d, idx]
+                for i in range(len(stackboard[x][y])):
+                    if stackboard[x][y][i] == idx:
+                        for j in stackboard[x][y][i:]:
+                            mal[j][0],mal[j][1] = nx,ny
+                        stackboard[nx][ny] += stackboard[x][y][i:]
+                        stackboard[x][y] = stackboard[x][y][0:i]
+                        break
+                # for i in stackboard:
+                #     print(i)
+                # print(mal)
+                # input(idx)
+
+            elif board[nx][ny] == 1:
+                mal[idx] = [nx, ny, d, idx]
+                for i in range(len(stackboard[x][y])):
+                    if stackboard[x][y][i] == idx:
+                        for j in stackboard[x][y][i:]:
+                            mal[j][0], mal[j][1] = nx, ny
+                        plus = stackboard[x][y][i:]
+                        stackboard[nx][ny] += plus[::-1]
+                        stackboard[x][y] = stackboard[x][y][0:i]
+                        break
+            for i in range(n):
+                for j in range(n):
+                    if len(stackboard[i][j]) >= 4:
+                        return cnt
+        # for i in stackboard:
+        #     print(i)
+        # print(mal)
+        # input(f"{cnt} 번 이동")
+    return -1
+
+n, m = map(int, input().split(' ')) ## 보드크기, 말 갯수
+board = [list(map(int,input().split())) for _ in range(n)]
+stackboard = [[[] for _ in range(n)] for _ in range(n)]
+mal = []
+
+for i in range(m):
+    x,y,d = map(int,input().split())
+    mal.append([x-1,y-1,d-1,i])
+    stackboard[x-1][y-1] = [i]
+
+# for i in stackboard:
+#     print(i)
+# print(mal)
+# print("초기상태")
+
+print(play())
